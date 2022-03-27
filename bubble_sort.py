600f3658eb71ad932916bac76070aaabd66305a2
Jodiardi Akbar <jodiardi.akbar@gmail.com>
2022-03-27 12:57:09 -0400

    Add files via upload

--- bubble_sort.py
+++ bubble_sort.py
@@ -5,5 +5,13 @@ def bubble_sort(arr):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
+def bubble_sort_excercise(arr):
+    for outer in range(len(arr) - 1):
+        print(f"bubble sort: {arr}")
+        for num in range(len(arr) - 1):
+            if arr[num] > arr[num + 1]:
+                arr[num], arr[num + 1] = arr[num + 1], arr[num]
+
+
 l = [6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5]
-bubble_sort(l)
+bubble_sort_excercise(l)
